cf1b1e6ac25e5f9b2bd88125d34eedc65180877f
M <779941@cognizant.com>
2020-03-11 16:47:32 +0530

    lab4-distributed

---
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -0,0 +1,35 @@
+package io.pivotal.pal.tracker.allocations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestOperations;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import java.util.TimeZone;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableOAuth2Client
+@EnableResourceServer
+@EnableWebSecurity
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan({"io.pivotal.pal.tracker.allocations", "io.pivotal.pal.tracker.restsupport"})
+public class App {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    ProjectClient projectClient(
+        RestOperations restOperations,
+        @Value("${registration.server.endpoint}") String registrationEndpoint
+    ) {
+        return new ProjectClient(restOperations, registrationEndpoint);
+    }
+}
---
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -0,0 +1,29 @@
+package io.pivotal.pal.tracker.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import java.util.TimeZone;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableOAuth2Client
+@EnableResourceServer
+@EnableWebSecurity
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan({
+    "io.pivotal.pal.tracker.accounts",
+    "io.pivotal.pal.tracker.restsupport",
+    "io.pivotal.pal.tracker.projects",
+    "io.pivotal.pal.tracker.users",
+    "io.pivotal.pal.tracker.registration"
+})
+public class App {
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(App.class, args);
+    }
+}
